d7119573400824fe15eae5586823ae6ffcfc1bec
mohammad-quanit <muhammadquanit@gmail.com>
2020-09-19 13:39:07 +0500

    e2e tests in angular

--- cypress/integration/DemoTest.spec.js
+++ cypress/integration/DemoTest.spec.js
@@ -1,8 +1,7 @@
-
 describe("Our Todo App Test Suite", () => {
-  it("Visiting our app", () => {
-    cy.visit("/")
-    cy.get(".nav-wrapper").contains("Items Manager")
+  it("Visiting our app", async () => {
+    cy.visit("/");
+    cy.get(".nav-wrapper").contains("Items Manager");
     cy.get("input[name='title']")
-  })
-})
+  });
+});
